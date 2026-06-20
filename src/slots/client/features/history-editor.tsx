63571f4d634f6ea121ae209157374ff37fd8398c
laotu <2427796523@qq.com>
2026-06-20 13:58:44 +0800

    add json editor

--- src/slots/client/features/history-editor.tsx
+++ src/slots/client/features/history-editor.tsx
@@ -3,7 +3,8 @@ import {useTranslations} from "next-intl";
 import {getSlotAndHistories, updateStoryHistory, useSlotContext} from "@/slots/client/models";
 import {handleHistoryPageChange, useHistoryPageState} from "@/slots/client/history-pager";
 import {
-    Dialog, DialogClose,
+    Dialog,
+    DialogClose,
     DialogContent,
     DialogFooter,
     DialogHeader,
@@ -14,11 +15,15 @@ import {Button} from "@/components/ui/button";
 import {EditIcon} from "lucide-react";
 import {SlotFeature} from "@/slots/client/feeature-models";
 import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/tooltip";
-import {useCallback, useState} from "react";
+import {useCallback, useRef, useState} from "react";
 import {StoryHistory} from "@/stories/models";
 import {Field, FieldGroup, FieldLabel, FieldSet} from "@/components/ui/field";
 import {Textarea} from "@/components/ui/textarea";
-
+import Editor, {OnMount} from '@monaco-editor/react';
+import {editor} from "monaco-editor";
+import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+import {toast} from "sonner";
+import {editorClassName} from "@/components/consts";
 
 export function HistoryEditor() {
     const {handleError} = useErrorHandler();
@@ -27,6 +32,14 @@ export function HistoryEditor() {
     const {page} = useHistoryPageState();
     const [history, setHistory] = useState<StoryHistory | undefined>(undefined);
     const [open, setOpen] = useState<boolean>(false);
+    const editorRef = useRef<IStandaloneCodeEditor>(null);
+
+    const handleEditorDidMount: OnMount = (editor) => {
+        // here is the editor instance
+        // you can store it in `useRef` for further usage
+        editorRef.current = editor;
+    }
+
     const handleDialogOpen = useCallback((open: boolean) => {
         try {
             if (open) {
@@ -43,9 +56,24 @@ export function HistoryEditor() {
     const handleHistoryUpdate = useCallback(async (data: FormData) => {
         try {
             console.debug('[HistoryEditor] open edit dialog')
+
+            if (!editorRef.current) {
+                return;
+            }
+
             const {histories, slot} = getSlotAndHistories(ctx);
             const index = useHistoryPageState.getState().page.cur - 1;
             const history = histories[index];
+            const variablesText = editorRef.current.getValue();
+            try {
+                history.variables = JSON.parse(variablesText);
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.error(error);
+                    toast.error(t("slot.variable_invalid_json") + '\r\n' + error.message);
+                    return;
+                }
+            }
             for (let i = 0; i < history.inputs.length; i++) {
                 const input = history.inputs[i];
                 input.content = data.get(`history_input-${i}`) as string;
@@ -78,14 +106,24 @@ export function HistoryEditor() {
                 </TooltipContent>
             </Tooltip>
         </DialogTrigger>
-        <DialogContent className={'w-11/12'}>
+        <DialogContent className="inline">
             {history && (
                 <form action={handleHistoryUpdate}>
                     <DialogHeader>
                         <DialogTitle>{t('slot.edit_history')}</DialogTitle>
                     </DialogHeader>
                     <FieldSet>
-                        <FieldGroup className={'max-h-[50vh] overflow-y-auto'}>
+                        <FieldGroup className={'p-1'}>
+                            <Field>
+                                <FieldLabel onClick={() => editorRef.current?.focus()}>
+                                    {t('slot.variable')}
+                                </FieldLabel>
+                                <Editor className={editorClassName} defaultLanguage={'json'} height={'10rem'}
+                                        defaultValue={JSON.stringify(history.variables)}
+                                        onMount={handleEditorDidMount}/>
+                            </Field>
+                        </FieldGroup>
+                        <FieldGroup className={'max-h-[50vh] overflow-y-auto p-1'}>
                             {history.inputs.map((u, i) => (
                                 <Field key={i}>
                                     <FieldLabel htmlFor={`history_input-${i}`}>
@@ -95,8 +133,6 @@ export function HistoryEditor() {
                                               name={`history_input-${i}`}
                                               id={`history_input-${i}`}/>
                                 </Field>))}
-                        </FieldGroup>
-                        <FieldGroup className={'max-h-[50vh] overflow-y-auto'}>
                             {history.outputs.map((u, i) => (
                                 <Field key={i}>
                                     <FieldLabel htmlFor={`history_output-${i}`}
