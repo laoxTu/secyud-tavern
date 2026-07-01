ab6714ad4ac8942c231d641a66ff461d53716e8d
laotu <2427796523@qq.com>
2026-07-01 21:15:44 +0800

    add text for event match

--- src/engines/lorebooks/match/event/client/date-editor.tsx
+++ src/engines/lorebooks/match/event/client/date-editor.tsx
@@ -1,20 +1,28 @@
 ﻿import {EventDate} from "@/engines/lorebooks/match/event/models";
-import {InputGroup, InputGroupInput} from "@/components/ui/input-group";
+import {InputGroup, InputGroupInput, InputGroupText} from "@/components/ui/input-group";
+import {useTranslations} from "next-intl";
 
 export function DateEditor({defaultValue, name, id}: { defaultValue: EventDate, name: string, id: string }) {
 
+    const t = useTranslations();
     return (
         <>
-            <InputGroup className="w-full" id={id}>
-                <InputGroupInput id={`${id}-year`} type={"number"}
-                                 min={0} step={1} name={`${name}-year`}
-                                 defaultValue={defaultValue.year}/>
-                <InputGroupInput id={`${id}-month`} type={"number"}
-                                 min={1} max={12} step={1} name={`${name}-month`}
-                                 defaultValue={defaultValue.month}/>
-                <InputGroupInput id={`${id}-day`} type={"number"}
-                                 min={1} max={31} step={1} name={`${name}-day`}
-                                 defaultValue={defaultValue.day}/>
+            <InputGroup className="w-full px-2" id={id}>
+                <InputGroupText>{t('default.year')} :</InputGroupText>
+                <InputGroupInput
+                    id={`${id}-year`} type={"number"}
+                    min={0} step={1} name={`${name}-year`}
+                    defaultValue={defaultValue.year}/>
+                <InputGroupText>{t('default.month')} :</InputGroupText>
+                <InputGroupInput
+                    id={`${id}-month`} type={"number"}
+                    min={1} max={12} step={1} name={`${name}-month`}
+                    defaultValue={defaultValue.month}/>
+                <InputGroupText>{t('default.day')} :</InputGroupText>
+                <InputGroupInput
+                    id={`${id}-day`} type={"number"}
+                    min={1} max={31} step={1} name={`${name}-day`}
+                    defaultValue={defaultValue.day}/>
             </InputGroup>
         </>
     );
